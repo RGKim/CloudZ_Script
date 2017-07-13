75bccb5c5786f10798af1a5110066a135fa1a7e1
RGKim <heehouse1@gmail.com>
2017-07-14 08:49:32 +0900

    log
    log

--- password_cent.sh
+++ password_cent.sh
@@ -6,10 +6,10 @@ systemctl start mysqld
 
 cd /var/www/redmine
 
-sthvar=$(pwgen 13 1)
+sthvar=$(pwgen 13 1) &> log.txt
 
-eval "RAILS_ENV=production bin/rails runner 'puts user = User.find(1); user.password, user.password_confirmation = \"$sthvar\"; user.save! '" > log.txt
+eval "RAILS_ENV=production bin/rails runner 'puts user = User.find(1); user.password, user.password_confirmation = \"$sthvar\"; user.save! '" &>> log.txt
 
-mkdir /ReadMe
+mkdir /ReadMe 
 echo "The Admin Password for Redmine is \""$sthvar"\"" > /ReadMe/ReadMe
 
