63395f83db526703810d0d9a6411372d34c8a423
root <root@web01.cloudtech03.sk.com>
2017-07-12 23:50:28 -0500

    password sh

---
+++ password.sh
@@ -0,0 +1,11 @@
+pt install pwgen
+
+cd /var/www/redmine
+
+sthvar=$(pwgen 8 1)
+
+echo "The Admin Password is \""$sthvar"\"" > ~/readme.txt
+
+
+eval "RAILS_ENV=production bin/rails runner 'puts user = User.find(1); user.password, user.password_confirmation = \"$sthvar\"; user.save! '"
+
