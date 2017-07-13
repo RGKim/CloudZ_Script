cf6bf770fbf3e916dcb7e20bfa37a997e4b53b91
RGKim <heehouse1@gmail.com>
2017-07-13 14:46:07 +0900

    update
    update

--- password.sh
+++ password.sh
@@ -4,7 +4,7 @@ cd /var/www/redmine
 
 sthvar=$(pwgen 8 1)
 
-echo "The Admin Password is \""$sthvar"\"" > ~/readme.txt
+echo "The Admin Password is \""$sthvar"\"" > ~/readme
 
 
 eval "RAILS_ENV=production bin/rails runner 'puts user = User.find(1); user.password, user.password_confirmation = \"$sthvar\"; user.save! '"
