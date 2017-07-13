da5e7c04799ab56748c4ef2e2cb60f1e65b3f325
RGKim <heehouse1@gmail.com>
2017-07-14 08:37:11 +0900

    make log
    log

--- password_cent.sh
+++ password_cent.sh
@@ -8,7 +8,7 @@ cd /var/www/redmine
 
 sthvar=$(pwgen 13 1)
 
-eval "RAILS_ENV=production bin/rails runner 'puts user = User.find(1); user.password, user.password_confirmation = \"$sthvar\"; user.save! '"
+eval "RAILS_ENV=production bin/rails runner 'puts user = User.find(1); user.password, user.password_confirmation = \"$sthvar\"; user.save! '" > log.txt
 
 mkdir /ReadMe
 echo "The Admin Password for Redmine is \""$sthvar"\"" > /ReadMe/ReadMe
