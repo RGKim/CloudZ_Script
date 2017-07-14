1265db13ae318a1bab02a1bba184bc4f6adc2390
RGKim <heehouse1@gmail.com>
2017-07-14 17:45:40 +0900

    Create gitlab-Ubuntu.sh

---
+++ gitlab-Ubuntu.sh
@@ -0,0 +1,8 @@
+sthvar=$(pwgen 8 1)
+
+mkdir /ReadMe
+
+echo "The Admin Password for GitLab is \""$sthvar"\"" > /ReadMe/ReadMe
+
+
+eval "gitlab-rails runner 'puts user = User.find(1); user.password, user.password_confirmation = \"$sthvar\"; user.save! '"
