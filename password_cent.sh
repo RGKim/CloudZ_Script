411d6120d8ccffc4c9500008765bbf367af0136d
RGKim <heehouse1@gmail.com>
2017-07-14 12:27:52 +0900

    pleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeease
    ppppppppppppplzzzz

--- password_cent.sh
+++ password_cent.sh
@@ -3,13 +3,10 @@
 systemctl start nginx
 systemctl start mysqld
 
-
-
 cd /var/www/redmine
 
-ln -s /usr/local/ruby/bin/ruby /usr/local/rvm/rubies/ruby-2.2.5/bin/ruby &> log.txt
-
-sthvar=$(pwgen 13 1) 
+sthvar=$(pwgen 13 1)
+echo $pwgen &> log.txt
 
 eval "RAILS_ENV=production bin/rails runner 'puts user = User.find(1); user.password, user.password_confirmation = \"$sthvar\"; user.save! '" &>> log.txt
 
