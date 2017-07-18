5ef455dd4bc4522b1beeb8761a6ff8330dff4d8f
RGKim <heehouse1@gmail.com>
2017-07-18 14:34:09 +0900

    Update magento2_centOS.sh

--- magento2_centOS.sh
+++ magento2_centOS.sh
@@ -1,7 +1,3 @@
-#reset password 
-yum install epel-release -y
-yum install pwgen -y
-
 cd /var/www/html
 
 sthvar=$(pwgen 8 1)
@@ -16,21 +12,15 @@ urla=$urla$(wget -qO- http://ipecho.net/plain ; echo)
 urla=$urla'/'
 
 
-echo "The Adress of Admin Page is \""&urla"admin\" >> /ReadMe/ReadMe
+echo "The Adress of Admin Page is \""$urla"admin\"" >> /ReadMe/ReadMe
 
-php /var/www/html/bin/magento setup:install --base-url="$urla" --backend-frontname=admin \
- --db-host=localhost --db-name=magento --db-user=magentouser --db-password=Magento@123 \
- --admin-firstname=Magento --admin-lastname=User --admin-email=admin@admin.com \
- --admin-user=admin --admin-password=$sthvar --language=en_US \
- --currency=USD --timezone=America/Chicago --use-rewrites=1
+php /var/www/html/bin/magento setup:install --base-url="$urla" --backend-frontname=admin\
+ --db-host=localhost --db-name=magento --db-user=magentouser --db-password=Magento@123\
+ --admin-firstname=Magento --admin-lastname=User --admin-email=admin@admin.com\
+ --admin-user=admin --admin-password=$sthvar --language=en_US\
+ --currency=USD --timezone=America/Chicago --use-rewrites=1 
 
-chmod -R g+w /var/www/html/{pub,var} && chmod -R g+w /var/www/html/{app/etc,vendor}
+chown -R :apache /var/www/html && find /var/www/html -type f -print0 | xargs -r0 chmod 640 && find /var/www/html -type d -print0 | xargs -r0 chmod 750 && chmod -R g+w /var/www/html/{pub,var} && chmod -R g+w /var/www/html/{app/etc,vendor}
 
+systemctl restart httpd
 
-
-
-
-
-/usr/bin/php /var/www/html/bin/magento cron:run | grep -v "Ran jobs by schedule" >> /var/www/html/var/log/magento.cron.log
-/usr/bin/php /var/www/html/update/cron.php >> /var/www/html/var/log/update.cron.log
-/usr/bin/php /var/www/html/bin/magento setup:cron:run >> /var/www/html/var/log/setup.cron.log
