544c3c63eb112b49676a1a2b7d4791e1e2526d4e
RGKim <heehouse1@gmail.com>
2017-07-25 08:59:01 +0900

    why
    why why

--- wildfly_ubuntu.sh
+++ wildfly_ubuntu.sh
@@ -1,7 +1,7 @@
 cd /opt/wildfly
 
-sthvar=$(pwgen 8 1)
+sthvar=$(pwgen 8 1) &> log.txt
 
-echo "The Admin ID is admin and the Password is \""$sthvar"\"" > Password.txt
+echo "The Admin ID is admin and the Password is \""$sthvar"\"" &> Password.txt
 
 ./bin/add-user.sh admin $sthvar
