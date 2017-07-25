cb5d3a3e38df23b95fa70fd62fa433e00581aa72
RGKim <heehouse1@gmail.com>
2017-07-25 14:00:44 +0900

    Create tomcat_centos.sh

---
+++ tomcat_centos.sh
@@ -0,0 +1,8 @@
+
+cd /opt/tomcat/conf
+
+sthvar=$(pwgen 8 1)
+
+echo "The Tomcat Admin Username is \"admin\" and Password is \""$sthvar"\"" > /ReadMe/ReadMe
+
+sed -i "44s/adminpwd/$sthvar/g" tomcat-users.xml
