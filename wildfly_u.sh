85c0a2e5382dd59c5a757a3546fb539c21cd7ad5
RGKim <heehouse1@gmail.com>
2017-07-25 09:44:29 +0900

    Create wildfly_u.sh

---
+++ wildfly_u.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+mkdir /ReadMe
+
+
+cd /opt/wildfly
+
+sthvar=$(pwgen 8 1)
+
+
+echo "The Admin ID is admin and the Password is \""$sthvar"\"" &> /ReadMe/ReadMe
+
+./bin/add-user.sh admin $sthvar
