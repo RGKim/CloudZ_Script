0dd2876d18570dfd14df62e1c8f326c218b64f1d
RGKim <heehouse1@gmail.com>
2017-07-25 15:11:49 +0900

    Create owncloud_centos.sh

---
+++ owncloud_centos.sh
@@ -0,0 +1,17 @@
+cd /var/www/html/owncloud/config
+
+
+url=$(wget -qO- http://ipecho.net/plain ; echo)
+
+sed -i "s/169.56.67.94/$url" config.php
+
+
+cd /var/www/html/owncloud
+
+sthvar=$(pwgen 8 1)
+export OC_PASS=$sthvar
+
+mkdir /ReadMe
+echo "The Admin of OwnCloud's Username is \"admin\" Password is \""$sthvar"\"" > /ReadMe/ReadMe
+
+su -s /bin/sh apache -c 'php ./occ user:resetpassword --password-from-env admin'
