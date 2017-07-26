ac45bc1f612d98cf6ade5c8aba4d24df5410b01e
RGKim <heehouse1@gmail.com>
2017-07-26 16:26:37 +0900

    Create gitlab-passwd.sh

---
+++ gitlab-passwd.sh
@@ -0,0 +1,44 @@
+
+#!/bin/bash
+
+cat << EOF > /root/cloudz.sh
+#!/bin/sh
+
+CONFIG_FILE="/root/provisioningConfiguration.cfg"
+if [ -f "\$CONFIG_FILE" ] ; then
+  source \$CONFIG_FILE
+
+  NEW_PASSWORD=\$OS_PASSWORD
+
+  systemctl start postfix
+  gitlab-ctl reconfigure
+  
+  gitlab-rails runner 'puts user = User.find(1); user.password, user.password_confirmation = \"$NEW_PASSWORD\"; user.save! '
+  
+
+  systemctl disable cloudz
+  rm -f /etc/systemd/system/cloudz.service
+  rm -f /root/cloudz.sh
+else
+  echo "provisioningConfiguration file not exist"
+fi
+EOF
+
+chmod 755 /root/cloudz.sh
+
+cat << EOF > /etc/systemd/system/cloudz.service
+[Unit]
+Description=CloudZ Install
+After=network.target
+
+[Service]
+ExecStart=/root/cloudz.sh
+Type=oneshot
+TimeoutSec=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable cloudz
+cat /dev/null > /root/.bash_history && history -c  
