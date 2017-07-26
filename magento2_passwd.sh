505caa9ba6c314d95c304c43f613ff1d4ec9d941
RGKim <heehouse1@gmail.com>
2017-07-26 15:23:31 +0900

    Update magento2_passwd.sh

--- magento2_passwd.sh
+++ magento2_passwd.sh
@@ -20,7 +20,7 @@ SET PASSWORD FOR 'root'@'localhost' = PASSWORD('\$NEW_PASSWORD');\
 SET PASSWORD FOR 'root'@'127.0.0.1' = PASSWORD('\$NEW_PASSWORD');\
 SET PASSWORD FOR 'root'@'::1' = PASSWORD('\$NEW_PASSWORD');\
 SET PASSWORD FOR 'magentouser'@'localhost' = PASSWORD('\$NEW_PASSWORD');\
-FLUSH PRIVILEGES;"
+FLUSH PRIVILEGES;" &> log.txt
 
 
 
@@ -31,7 +31,7 @@ php /var/www/html/bin/magento setup:install --base-url="http://\$MYIP/" --backen
  --db-host=localhost --db-name=magento --db-user=magentouser --db-password=\$OS_PASSWORD\
  --admin-firstname=CloudZ --admin-lastname=User --admin-email=admin@admin.com\
  --admin-user=CloudZ --admin-password=\$OS_PASSWORD --language=en_US\
- --currency=USD --timezone=America/Chicago --use-rewrites=1 
+ --currency=USD --timezone=America/Chicago --use-rewrites=1 &>> log.txt
 
 chown -R :apache /var/www/html && find /var/www/html -type f -print0 | xargs -r0 chmod 640 && find /var/www/html -type d -print0 | xargs -r0 chmod 750 && chmod -R g+w /var/www/html/{pub,var} && chmod -R g+w /var/www/html/{app/etc,vendor}
 
