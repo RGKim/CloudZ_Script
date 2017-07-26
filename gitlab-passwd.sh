f2b17b513a1a132c4b19c94a00feaa152662dd29
RGKim <heehouse1@gmail.com>
2017-07-26 17:36:36 +0900

    Update gitlab-passwd.sh

--- gitlab-passwd.sh
+++ gitlab-passwd.sh
@@ -10,6 +10,30 @@ if [ -f "\$CONFIG_FILE" ] ; then
 
   NEW_PASSWORD=\$OS_PASSWORD
 
+
+/usr/bin/mysqladmin -u root password "\$NEW_PASSWORD"
+SECURE_MYSQL=\$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"$INIT_PASSWORD\r\"
+expect \"Change the root password?\"
+send \"y\r\"
+expect \"New password\"
+send \"$INIT_PASSWORD\r\"
+expect \"Re-enter new password\"
+send \"$INIT_PASSWORD\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+
   systemctl start postfix
   gitlab-ctl reconfigure
   
