6f51e6b3473da331bc7442d847d5afd2a9b2558b
RGKim <heehouse1@gmail.com>
2017-07-31 16:55:53 +0900

    Create redmine_ubuntu_pw.sh

---
+++ redmine_ubuntu_pw.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+cat << EOF > /root/cloudz.sh
+#!/bin/sh
+
+CONFIG_FILE="/root/provisioningConfiguration.cfg"
+if [ -f "\$CONFIG_FILE" ] ; then
+  source \$CONFIG_FILE
+
+  OLD_PASSWORD="admin"
+  NEW_PASSWORD=\$OS_PASSWORD
+  
+  service mariadb start
+  service nginx start
+  
+  /usr/bin/mysql -u root -p\$OLD_PASSWORD mysql -e "\
+  SET PASSWORD FOR 'root'@'localhost' = PASSWORD('\$NEW_PASSWORD');\
+  SET PASSWORD FOR 'root'@'127.0.0.1' = PASSWORD('\$NEW_PASSWORD');\
+  SET PASSWORD FOR 'root'@'::1' = PASSWORD('\$NEW_PASSWORD');\
+  SET PASSWORD FOR 'redmine'@'localhost' = PASSWORD('\$NEW_PASSWORD');\
+  FLUSH PRIVILEGES;"
+  
+  cd /var/www/redmine
+  
+  sed -i "s/\$OLD_PASSWORD/\$NEW_PASSWORD/g" /var/www/redmine/config/database.yml
+  
+  mkdir -p tmp tmp/pdf public/plugin_assets
+  chown -R nobody:nobody files log tmp public/plugin_assets
+  chmod -R 775 files log tmp public/plugin_assets
+  
+  gem install bundler
+  bundle install --without development test
+  
+  bundle exec rake generate_secret_token
+  RAILS_ENV=production bundle exec rake db:migrate
+  
+  eval "RAILS_ENV=production bin/rails runner 'puts user = User.find(1); user.password, user.password_confirmation = \"\$NEW_PASSWORD\"; user.save! '"
+
+  service nginx restart
+  
+  service cloudz disable
+  
+  rm -f /etc/systemd/system/cloudz.service
+  rm -f /root/cloudz.sh
+else
+  echo "provisioningConfiguration file not exist"
+fi
+EOF
+
+chmod 755 /root/cloudz.sh
+
+cat << EOF > /etc/systemd/system/cloudz.service
+[Unit]
+Description=CloudZ Install
+After=network.target
+
+[Service]
+ExecStart=/root/cloudz.sh
+Type=oneshot
+TimeoutSec=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+service enable cloudz
+cat /dev/null > /root/.bash_history && history -c  
