ef2940568954f8302feb992a45f9917be4593993
RGKim <heehouse1@gmail.com>
2017-07-31 14:54:45 +0900

    Update redmine-passwd.sh

--- redmine-passwd.sh
+++ redmine-passwd.sh
@@ -20,7 +20,9 @@ if [ -f "\$CONFIG_FILE" ] ; then
   SET PASSWORD FOR 'redmine'@'localhost' = PASSWORD('\$NEW_PASSWORD');\
   FLUSH PRIVILEGES;"
 
-  sed -i "s/'\$OLD_PASSWORD'/'\$NEW_PASSWORD'/g" /var/www/redmine/config/database.yml
+  sed -i "s/\$OLD_PASSWORD/\$NEW_PASSWORD/g" /var/www/redmine/config/database.yml
+  
+  cd /var/www/redmine
   
   eval "RAILS_ENV=production bin/rails runner 'puts user = User.find(1); user.password, user.password_confirmation = \"\$NEW_PASSWORD\"; user.save! '"
 
