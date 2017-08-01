9b063604b09c971ac8ebd86291cdf028c37e449a
RGKim <heehouse1@gmail.com>
2017-08-01 13:43:32 +0900

    Create boot_tomcat_passwd.sh

---
+++ boot_tomcat_passwd.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+cat << EOF > /root/cloudz.sh
+#!/bin/sh
+
+CONFIG_FILE="/root/provisioningConfiguration.cfg"
+if [ -f "\$CONFIG_FILE" ] ; then
+  source \$CONFIG_FILE
+
+  OLD_PASSWORD="admin"
+  NEW_PASSWORD=\$OS_PASSWORD
+
+  systemctl start mariadb
+  
+  /usr/bin/mysql -u root -p\$OLD_PASSWORD mysql -e "\
+  SET PASSWORD FOR 'root'@'localhost' = PASSWORD('\$NEW_PASSWORD');\
+  SET PASSWORD FOR 'root'@'127.0.0.1' = PASSWORD('\$NEW_PASSWORD');\
+  SET PASSWORD FOR 'root'@'::1' = PASSWORD('\$NEW_PASSWORD');\
+  FLUSH PRIVILEGES;"
+  
+  cd /opt/tomcat/conf
+  
+  sed -i "44s/adminpwd/\$NEW_PASSWORD/g" tomcat-users.xml
+
+  
+  systemctl disable cloudz
+  rm -f /etc/systemd/system/cloudz.service
+  rm -f /root/cloudz.sh
+else
+  echo "provisioningConfiguration file not exist"
+fi
+EOF
+chmod 755 /root/cloudz.sh
+
+cat << EOF > /etc/systemd/system/cloudz.service
+[Unit]
+Description=CloudZ Install
+After=network.target
+
+[Service]
+ExecStart=/root/cloudz.sh
+Type=oneshot
+TimeoutSec=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable cloudz
+cat /dev/null > /root/.bash_history && history -c
