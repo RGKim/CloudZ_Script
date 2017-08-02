3b3cfecfdfa9cfc899c6b5a4fd6660c28a94a20c
RGKim <heehouse1@gmail.com>
2017-08-02 13:21:06 +0900

    Update boot_mattermost_passwd.sh

--- boot_mattermost_passwd.sh
+++ boot_mattermost_passwd.sh
@@ -1,6 +1,55 @@
-sed -i "s/'\$OLD_PASSWORD'/'\$NEW_PASSWORD'/g" /opt/mattermost/config/config.json
-	
+#!/bin/bash
 
-cd /opt/mattermost/bin
+cat << EOF > /root/cloudz.sh
+#!/bin/sh
 
-./platform user password admin@cloudz.co.kr \$NEW_PASSWORD
+CONFIG_FILE="/root/provisioningConfiguration.cfg"
+if [ -f "\$CONFIG_FILE" ] ; then
+  source \$CONFIG_FILE
+
+  OLD_PASSWORD="admin"
+  NEW_PASSWORD=\$OS_PASSWORD
+  
+  systemctl start mariadb
+  
+  /usr/bin/mysql -u root -p\$OLD_PASSWORD mysql -e "\
+  SET PASSWORD FOR 'root'@'localhost' = PASSWORD('\$NEW_PASSWORD');\
+  SET PASSWORD FOR 'root'@'127.0.0.1' = PASSWORD('\$NEW_PASSWORD');\
+  SET PASSWORD FOR 'root'@'::1' = PASSWORD('\$NEW_PASSWORD');\
+  SET PASSWORD FOR 'mmuser'@'localhost' = PASSWORD('\$NEW_PASSWORD');\
+  FLUSH PRIVILEGES;"
+  
+  sed -i "s/'\$OLD_PASSWORD'/'\$NEW_PASSWORD'/g" /opt/mattermost/config/config.json
+  
+  cd /opt/mattermost/bin
+  
+  ./platform user password admin@cloudz.co.kr \$NEW_PASSWORD
+
+  systemctl disable cloudz
+  rm -f /etc/systemd/system/cloudz.service
+  rm -f /root/cloudz.sh
+  
+else
+  echo "provisioningConfiguration file not exist"
+fi
+
+EOF
+
+chmod 755 /root/cloudz.sh
+
+cat << EOF > /etc/systemd/system/cloudz.service
+[Unit]
+Description=CloudZ Install
+After=network.target
+
+[Service]
+ExecStart=/root/cloudz.sh
+Type=oneshot
+TimeoutSec=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable cloudz
+cat /dev/null > /root/.bash_history && history -c  
